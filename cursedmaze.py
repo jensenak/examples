51d80d2bdc053a04ee575679301a67b402ad1626
Adam Jensen <adam.jensen@kount.com>
2016-09-20 22:03:14 -0600

    Cursed maze works... just needs curses!

--- cursedmaze.py
+++ cursedmaze.py
@@ -1,20 +1,46 @@
+#from curses import wrapper
 import sys
 
 class InputError(Exception):
     pass
 
-map = [[0,0,0,0,0,0,0,0,0,0],
-       [0,1,1,4,0,0,0,0,0,0],
-       [0,3,1,4,3,3,6,5,0,0],
-       [0,1,1,3,1,1,1,1,0,0],
-       [0,1,6,5,1,5,6,1,0,0],
-       [0,1,1,1,1,6,6,1,4,0],
-       [0,6,6,3,4,0,4,1,1,0],
-       [0,0,0,0,0,0,0,5,1,0],
-       [0,0,0,0,0,0,2,1,1,0],
-       [0,0,0,0,0,0,0,0,0,0]]
 
-location = {"x": 1, "y": 1}
+class Dungeon():
+    def __init__(self, src, states):
+        self.level = src
+        self.states = states
+
+    def fate(self, user):
+        state = self.level[user.y][user.x]
+        print(self.states[state]["words"])
+        if self.states[state]["dead"]:
+            user.respawn()
+        if self.states[state]["win"]:
+            sys.exit()
+
+
+class Player():
+    def __init__(self, name, spawn):
+        self.name = name
+        self.spawn = spawn
+        self.respawn()
+
+    def respawn(self):
+        print()
+        self.x = self.spawn['x']
+        self.y = self.spawn['y']
+
+    def move(self, dir):
+        if dir == "n":
+            self.y -= 1
+        if dir == "s":
+            self.y += 1
+        if dir == "e":
+            self.x += 1
+        if dir == "w":
+            self.x -= 1
+
+
 def parseInput(s):
     if len(s) == 0:
         raise InputError()
@@ -25,48 +51,32 @@ def parseInput(s):
         sys.exit()
     raise InputError()
 
-def move(dir):
-    if dir == "n":
-        location["y"] -= 1
-    if dir == "s":
-        location["y"] += 1
-    if dir == "e":
-        location["x"] += 1
-    if dir == "w":
-        location["x"] -= 1
-    return test()
+stateMap = [
+    {"dead": False, "win": False, "words": "Keep going"},
+    {"dead": False, "win": True, "words": "You are the winner of the Universe!"},
+    {"dead": True, "win": False, "words": "You just became tasty lunch for a goblin"},
+    {"dead": True, "win": False, "words": "Your face melted ... IN A POOL OF LAVA"},
+    {"dead": True, "win": False, "words": "You are now a smudge on the bottom of a large boulder"},
+    {"dead": True, "win": False, "words": "You have been dissolved in acid"},
+    {"dead": True, "win": False, "words": "Oh look, you've been impaled"},
+]
+level = [[2,2,2,2,2,2],
+         [3,0,3,2,2,2],
+         [4,0,0,4,2,2],
+         [5,0,5,0,1,2],
+         [6,0,0,0,5,2],
+         [2,6,2,3,4,2]]
 
-def test():
-    return map[location["y"]][location["x"]]
+def main():
+    p = Player(input("What is your name? "), {"x": 1, "y": 1})
+    d = Dungeon(level, stateMap)
 
-def fate(state):
-    if state == 1:
-        print("Still alive... keep going.")
-        return False # still in maze
-    if state == 0:
-        print("You fell in a really big hole!")
-    if state == 2:
-        print("You are the winner of the universe!")
-        sys.exit()
-    if state == 3:
-        print("You just became tasty lunch for a goblin")
-    if state == 4:
-        print("Your face melted ... IN A POOL OF LAVA")
-    if state == 5:
-        print("You are now a smudge on the bottom of a large boulder")
-    if state == 6:
-        print("You have been dissolved in acid")
-    return True
+    while True:
+        try:
+            dir = parseInput(input("North, South, East, or West (q to quit)? "))
+            p.move(dir)
+            d.fate(p)
+        except InputError:
+            print("That's not a valid direction")
 
-while True:
-    try:
-        dir = parseInput(input("North, South, East, or West (q to quit)? "))
-        state = move(dir)
-        if fate(state):
-            location["x"] = 1
-            location["y"] = 1
-            print()
-    except InputError:
-        print("That's not a valid direction")
-
-input("Press Enter to continue")
+main()
