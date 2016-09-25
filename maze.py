6d49d93cfa71b5627a7bd8d8512254560d28ca99
Adam Jensen <adam.jensen@kount.com>
2016-09-24 18:23:23 -0600

    Be smarter about maze resolution

--- maze.py
+++ maze.py
@@ -3,18 +3,25 @@ import sys
 class InputError(Exception):
     pass
 
-map = [[0,0,0,0,0,0,0,0,0,0],
-       [0,1,1,4,0,0,0,0,0,0],
-       [0,3,1,4,3,3,6,5,0,0],
-       [0,1,1,3,1,1,1,1,0,0],
-       [0,1,6,5,1,5,6,1,0,0],
-       [0,1,1,1,1,6,6,1,4,0],
-       [0,6,6,3,4,0,4,1,1,0],
-       [0,0,0,0,0,0,0,5,1,0],
-       [0,0,0,0,0,0,2,1,1,0],
-       [0,0,0,0,0,0,0,0,0,0]]
+stateMap = [
+    {"dead": False, "win": False, "words": "Keep going"},
+    {"dead": False, "win": True, "words": "You are the winner of the Universe!"},
+    {"dead": True, "win": False, "words": "You just became tasty lunch for a goblin"},
+    {"dead": True, "win": False, "words": "Your face melted ... IN A POOL OF LAVA"},
+    {"dead": True, "win": False, "words": "You are now a smudge on the bottom of a large boulder"},
+    {"dead": True, "win": False, "words": "You have been dissolved in acid"},
+    {"dead": True, "win": False, "words": "Oh look, you've been impaled"},
+]
+
+level = [[2,2,2,2,2,2],
+         [3,0,3,2,2,2],
+         [4,0,0,4,2,2],
+         [5,0,5,0,1,2],
+         [6,0,0,0,5,2],
+         [2,6,2,3,4,2]]
 
 location = {"x": 1, "y": 1}
+
 def parseInput(s):
     if len(s) == 0:
         raise InputError()
@@ -34,38 +41,20 @@ def move(dir):
         location["x"] += 1
     if dir == "w":
         location["x"] -= 1
-    return test()
-
-def test():
-    return map[location["y"]][location["x"]]
-
-def fate(state):
-    if state == 1:
-        print("Still alive... keep going.")
-        return False # still in maze
-    if state == 0:
-        print("You fell in a really big hole!")
-    if state == 2:
-        print("You are the winner of the universe!")
-        sys.exit()
-    if state == 3:
-        print("You just became tasty lunch for a goblin")
-    if state == 4:
-        print("Your face melted ... IN A POOL OF LAVA")
-    if state == 5:
-        print("You are now a smudge on the bottom of a large boulder")
-    if state == 6:
-        print("You have been dissolved in acid")
-    return True
+    return level[location["y"]][location["x"]]
 
 while True:
     try:
         dir = parseInput(input("North, South, East, or West (q to quit)? "))
         state = move(dir)
-        if fate(state):
-            location["x"] = 1
-            location["y"] = 1
-            print()
+        print(stateMap[state]["words"])
+        if stateMap[state]["dead"]:
+            location['x'] = 1
+            location['y'] = 1
+            print("------- start over -------")
+            continue
+        if stateMap[state]["win"]:
+            break
     except InputError:
         print("That's not a valid direction")
 
