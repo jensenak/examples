e26db31c875d06440be102b6bd64ae9440cdb4cb
Adam Jensen <jensen.ak@gmail.com>
2016-10-18 15:43:24 -0600

    Coins

--- graphics.py
+++ graphics.py
@@ -17,36 +17,45 @@ class Player():
         self.y = random.randint(25, height - 25) 
         self.tail = [(self.x, self.y)]
         self.length = 16 
-        self.dir = (0, 1)
+        self.dx = 0
+        self.dy = -1
     
     def trim(self):
         if len(self.tail) <= self.length:
             return
         cut = len(self.tail) - self.length
         for px, py in self.tail[:cut]:
-            win.set_at((px, py), bg)
+            point(px, py, bg)
         self.tail = self.tail[cut:]
     
     def turn(self, key):
-        self.dir = self.vectors[key]
+        self.dx, self.dy = self.vectors[key]
 
     def grow(self, n):
         self.length += n
 
     def update(self):
-        dx, dy = self.dir
-        self.x += dx
-        self.y += dy 
-        if collision(self.x, self.y):
+        self.x += self.dx
+        self.y += self.dy 
+        if collision(self, self.x, self.y):
             return False
         self.tail.append((self.x, self.y))
-        win.set_at((self.x, self.y), self.color)
+        point(self.x, self.y, self.color)
         self.trim()
         return True
 
+def point(x, y, c):
+    win.set_at((x, y), c)
+    pygame.display.update(pygame.rect.Rect(x, y, 1, 1))
+    return
 
-width = int(input("Enter width "))
-height = int(input("Enter height "))
+def coin():
+    cx = random.randint(10, width - 10)
+    cy = random.randint(10, height - 10)
+    point(cx, cy, cg)
+    
+width = 400 #int(input("Enter width "))
+height = 300 #int(input("Enter height "))
 
 vectors = [{
     pygame.K_w: (0, -1),
@@ -62,32 +71,33 @@ vectors = [{
 
 players = []
 for i in range(2):
-    n = input("Enter player {} name ".format(i))
-    cr = int(input("Enter player {} red ".format(i)))
-    cg = int(input("Enter player {} green ".format(i)))
-    cb = int(input("Enter player {} blue ".format(i)))
+    n = "p{}".format(i) #input("Enter player {} name ".format(i))
+    cr = 255 - (i*255) #int(input("Enter player {} red ".format(i)))
+    cg = 0 #int(input("Enter player {} green ".format(i)))
+    cb = i*255 #int(input("Enter player {} blue ".format(i)))
     players.append(Player(n, pygame.color.Color(cr, cg, cb), vectors[i]))
 
 win = pygame.display.set_mode((width, height))
 bg = pygame.color.Color(0,0,0,255)
+cg = pygame.color.Color(255,255,255,255)
 
 def init():
-    win.fill(pygame.color.Color(255,255,255,255))
+    win.fill(pygame.color.Color(255,255,0,255))
     win.fill(bg, rect=pygame.Rect((2, 2, width-4, height-4)))
     pygame.display.flip()
 
 
-def collision(x, y):
+def collision(p, x, y):
     c = win.get_at((x, y))
+    if c == cg:
+        p.grow(4)
+        return False
     if c != bg:
         return True
         
 
 def run():
-    ticker = lambda: int(round(time.time() * 1000))
-    delay = 50
-    timer = ticker() + delay
-    
+    init()
     running = True
     while running:
         event = pygame.event.poll()
@@ -99,17 +109,15 @@ def run():
             elif event.key in vectors[1].keys():
                 players[1].turn(event.key)
     
-        if timer < ticker():
-            timer = ticker() + delay 
-            if not players[0].update():
-                players[0].reset(False)
-                players[1].reset(True)
-                init()
-            if not players[1].update():
-                players[1].reset(False)
-                players[0].reset(True)
-                init()
-        pygame.display.flip() 
+        if random.randint(1, 20) == 1:
+            coin()
+        if not players[0].update():
+            players[0].reset(False)
+            players[1].reset(True)
+            init()
+        if not players[1].update():
+            players[1].reset(False)
+            players[0].reset(True)
+            init()
 
-init()
 run()
